8ec4ff7fd5e013e3f2deefc2738b0f1e0e6eacae
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-11-06 00:38:38 -0500

    update chapter1

---
+++ Chap1_Introducction_to_nupy/loop1.py
@@ -0,0 +1,10 @@
+import numpy  as np
+
+list1 = [1, 2, 3]
+arr1 = np.array([1, 2, 3])
+
+for e in list1:
+    print(e)
+
+for e in arr1:
+    print(e)
---
+++ Chap1_Introducction_to_nupy/multiply1.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+list1 = [1, 2, 3]
+arr1 = np.array([1, 2, 3])
+
+print('arr1:    ', arr1)
+print('list1:   ', list1)
+print('2*arr1:  ', 2*arr1)
+print('2*list1: ', 2*list1)
