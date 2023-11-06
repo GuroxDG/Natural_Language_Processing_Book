5618dbb3d8ea104b6b1d36a79cbec3f89bc7ae80
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-11-06 01:07:02 -0500

    Create mathops_array1.py

---
+++ Chap1_Introducction_to_numpy/mathops_array1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+sqrt = np.sqrt(arr1)
+log1 = np.log(arr1)
+exp1 = np.exp(arr1)
+
+print('sqrt :', sqrt)
+print('log1 :', log1)
+print('exp1 :', exp1)
