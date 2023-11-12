ce86abb671e81d0d321616195a4dec853d51f086
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-11-11 23:59:22 -0500

    Update pandas_boolean_df.py

--- Chap2_Introductio_to_pandas/pandas_boolean_df.py
+++ Chap2_Introductio_to_pandas/pandas_boolean_df.py
@@ -11,3 +11,20 @@ print(df1 | df2)
 
 print("df1 ^ df2: negation")
 print(df1 ^ df2)
+print()
+
+print('Transposing')
+
+df1 = pd.DataFrame({'a': [1, 0, 1], 'b': [0, 1, 1] },
+dtype=int)
+print(df1)
+print("df1.T:")
+print(df1.T)
+print()
+
+print('Sum')
+
+df1 = pd.DataFrame({'a' : [1, 0, 1], 'b' : [0, 1, 1] }, dtype=int)
+df2 = pd.DataFrame({'a' : [3, 3, 3], 'b' : [5, 5, 5] }, dtype=int)
+print("df1 + df2:")
+print(df1 + df2)
